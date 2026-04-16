34f80c9bc4a053ba5fce692a9001da9807413ed4
a-shashko <a.shashko@gmail.com>
2026-04-16 19:19:02 +0300

    Minor changes

--- lesson_25.py
+++ lesson_25.py
@@ -2,7 +2,6 @@
 # import math
 # print(dir(math))
 # # print(help(math))
-
 # Задача: «SEO-таймер и логирование»
 # Тебе нужно написать программу, которая имитирует начало работы парсера.
 # Условие:
@@ -15,18 +14,14 @@
 #     Внутри этой папки создай файл run_log.txt.
 #     Запиши в этот файл строку с точным временем запуска в формате: [ЧАСЫ:МИНУТЫ:СЕКУНДЫ] Сессия запущена.
 #     Важно: Открывай файл в режиме дозаписи ('a'), чтобы при каждом запуске скрипта в файл добавлялась новая строка, а не стиралась старая.
+# import os
+# import datetime
+# now = datetime.datetime.now()
+# formated_date = now.strftime('%d_%m_%Y')
+# formated_time = now.strftime('%H:%M:%S')
+# if not os.path.exists(formated_date):
+#     os.makedirs(formated_date)
+# file_path = os.path.join(formated_date, 'run_log.txt')
+# with open(file_path, 'a', encoding='utf-8') as file:
+#     file.write(f'[{formated_time}] Сессия запущена\n')
 
-import os
-import datetime
-
-now = datetime.datetime.now()
-formated_date = now.strftime('%d_%m_%Y')
-formated_time = now.strftime('%H:%M:%S')
-
-if not os.path.exists(formated_date):
-    os.makedirs(formated_date)
-
-file_path = os.path.join(formated_date, 'run_log.txt')
-
-with open(file_path, 'a', encoding='utf-8') as file:
-    file.write(f'[{formated_time}] Сессия запущена\n')
